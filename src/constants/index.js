5d3cd2eaac2153633d44550d26ea498670a6ad28
William G. Onyami <Imayno85@gmail.com>
2025-12-28 17:12:23 +0300

    Update constants configuration

--- src/constants/index.js
+++ src/constants/index.js
@@ -132,7 +132,7 @@ export const feedback = [
     id: "feedback-1",
     content:
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Eusuph KIYINGI ANYWAR",
+    name: "Eusuph KIYINGI",
     title: "Founder & Managing Director",
     img: people01,
     url: "https://www.linkedin.com/in/yusuf-sserunkuma?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
